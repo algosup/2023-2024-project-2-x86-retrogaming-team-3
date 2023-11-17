8dd61b637daab9a796361f117bcbeac3b568ed19
Quentin-Clement <quentin.clement@algosup.com>
2023-11-17 16:17:11 +0100

    create cherry sprite

--- pacMan/buildSprite.bash
+++ pacMan/buildSprite.bash
@@ -1,7 +1,7 @@
 set -Cue
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
-ROOT_DIR="$(cd "${THIS_DIR}/../../.." && pwd)"
+ROOT_DIR="$(cd "${THIS_DIR}" && pwd)"
 BIN_DIR="${ROOT_DIR}/bin"
 
 echo "ROOT_DIR: ${ROOT_DIR}"
--- pacMan/buildSprite.cmd
+++ pacMan/buildSprite.cmd
@@ -1,6 +1,6 @@
 @echo off
 set "SCRIPT_DIR=%~dp0"
-set "ROOT_DIR=%SCRIPT_DIR%..\..\.."
+set "ROOT_DIR=%SCRIPT_DIR%"
 set "NASM=%ROOT_DIR%\nasm\nasm"
 set "BIN_DIR=%ROOT_DIR%\bin"
 
